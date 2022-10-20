a041339ad81367058ba37b5c19ea091c33f12682
Ivanova Stacey <stacey_ivanova@mail.ru>
2022-10-21 00:07:35 +0300

    test1

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -20,7 +20,7 @@ module.exports.createCard = (req, res, next) => {
 
 module.exports.findAllCards = (req, res, next) => {
   Card.find({})
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.send({ card }))
     .catch((err) => {
       next(err);
     });
