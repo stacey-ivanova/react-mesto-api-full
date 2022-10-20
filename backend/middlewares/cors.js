1378e82c83f480e9ca6f70e405faa54a80959818
Ivanova Stacey <stacey_ivanova@mail.ru>
2022-10-20 03:36:14 +0300

    change cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -14,11 +14,9 @@ const cors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
   } if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  } if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-
   next();
 };
 module.exports = {
