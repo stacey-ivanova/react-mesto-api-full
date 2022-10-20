fd062615a499cd29039d51ef11e59523d6d667f0
Ivanova Stacey <stacey_ivanova@mail.ru>
2022-10-20 13:28:11 +0300

    change cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,24 +1,24 @@
 const allowedCors = [
   'https://mesto.stacey.nomoredomains.icu',
+  'http://mesto.stacey.nomoredomains.icu',
   'https://api.mesto.stacey.nomoredomains.icu',
+  'http://api.mesto.stacey.nomoredomains.icu',
   'localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-const cors = (req, res, next) => {
+module.exports.cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   } if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
   next();
-};
-module.exports = {
-  cors,
 };
