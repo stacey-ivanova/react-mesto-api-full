97d113f3f5756a23db8c1b524a2286558de4e44c
Ivanova Stacey <stacey_ivanova@mail.ru>
2022-10-21 17:25:58 +0300

    test 43

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -71,7 +71,7 @@ module.exports.dislikeCard = (req, res, next) => {
     { $pull: { likes: req.user } },
     { new: true },
   )
-  // .populate(['owner', 'likes'])
+    .populate(['owner', 'likes'])
     .then((card) => {
       if (!card) {
         throw new NotFoundError('Передан несуществующий _id карточки');
